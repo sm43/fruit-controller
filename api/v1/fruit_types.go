66f082681983127ff1dcd62b1c82047077c3d630
SM43 <smukhade@redhat.com>
2021-04-15 00:20:05 +0530

    Creates CRD [make manifests]

--- api/v1/fruit_types.go
+++ api/v1/fruit_types.go
@@ -28,14 +28,19 @@ type FruitSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Fruit. Edit Fruit_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Name is the name of fruit
+	Name string `json:"name,omitempty"`
+	// Number is the number of instance of pods
+	Number uint `json:"number,omitempty"`
 }
 
 // FruitStatus defines the observed state of Fruit
 type FruitStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Status of fruit created 
+	Created bool `json:"created,omitempty"`
 }
 
 // +kubebuilder:object:root=true
